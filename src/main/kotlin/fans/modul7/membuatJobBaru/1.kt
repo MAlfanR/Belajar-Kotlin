0986e9c9a9cc685b4d66fd9304d72b66f689eb5d
MAlfanR <alfanriyadi25@gmail.com>
2020-07-23 22:26:16 +0700

    Coroutines3

---
+++ src/main/kotlin/fans/modul7/membuatJobBaru/1.kt
@@ -0,0 +1,14 @@
+package fans.modul7.membuatJobBaru
+
+import kotlinx.coroutines.*
+
+
+fun main() = runBlocking {
+    val job = launch(start = CoroutineStart.LAZY) {
+        delay(1000L)
+        println("Start new job!")
+    }
+
+    job.start()
+    println("Other task")
+}
